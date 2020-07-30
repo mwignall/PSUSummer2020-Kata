7dd84f3c752b4d5ddefc142159390e88ced04cc2
Mark Wignall <mwignall1967@gmail.com>
2020-07-29 19:25:51 -0700

    initial Start

---
+++ minesweeper/src/main/java/edu/pdx/cs410J/mwignall/Kata.java
@@ -0,0 +1,18 @@
+package edu.pdx.cs410J.mwignall;
+
+/**
+ * A class for getting started with a code kata
+ *
+ * Use IntelliJ's "Refactor | Rename..." command to change the name of this
+ * class (and its tests).
+ *
+ * Students: Mark Wignall
+ */
+public class Kata {
+                                                                                    
+
+  public static void main(String[] args) {
+    System.err.println("Missing command line arguments");
+    System.exit(1);
+  }
+}
